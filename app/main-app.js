e0ff9537ba93095ce204ff12dd8d711e62d87c52
Fafowora Olasunkanmi <sunkanmifafowora@gmail.com>
2019-12-25 12:06:52 +0100

    Fixed error in local storage for storing values

--- app/main-app.js
+++ app/main-app.js
@@ -2,12 +2,50 @@
 "use strict";
 
 const create_new_task_button = document.querySelector("#create-new-task");
-let user_tasks = document.querySelector("#user-tasks");
+const user_tasks = document.querySelector("#user-tasks");
+
+const Counter = function () {
+    let customCounter = 0;
+
+    return {
+        increment: function () {
+            customCounter++;
+        },
+        setValue: function(newVal){
+            customCounter = newVal;
+        },
+        value: function () {
+            return customCounter;
+        }
+    };
+};
+
+const DictVals = function () {
+    let cusDic = {};
+    return {
+        makeDict: function () {
+            return cusDic;
+        },
+        setDict: function (obj) {
+            cusDic = obj;
+        }
+    };
+};
+
+const myTasksDictFunc = DictVals();
+const myTasksCounter = Counter();
+const mySubTasksCounter = Counter();
+const mySubTasksDict = DictVals().makeDict();
+const completeTasksCounter = Counter();
+const completeTasksDict = DictVals().makeDict();
+const completeSubTasksCounter = Counter();
+const completeSubTasksDict = DictVals().makeDict();
 
 if (typeof (Storage) === "undefined") {
     console.log("Storage not supported");
 } else if (localStorage.getItem("user_tasks") != null) {
     user_tasks.innerHTML = JSON.parse(localStorage.getItem("user_tasks"));
+    DisplayUserValues();
 }
 
 create_new_task_button.addEventListener("click", () => {
@@ -46,19 +84,17 @@ create_new_task_button.addEventListener("click", () => {
 
     user_tasks.insertAdjacentHTML("beforeend", user_inputs);
 
-    user_tasks = document.querySelector("#user-tasks");
     localStorage.setItem("user_tasks", JSON.stringify(user_tasks.innerHTML));
 });
 
-function ChangeColor(element){
+function ChangeColor(element) {
     const priorityButtonParentNode = element.parentNode;
     const priorityLvlNode = priorityButtonParentNode.children[1];
 
-    if(priorityLvlNode.hasAttribute("disabled")){
+    if (priorityLvlNode.hasAttribute("disabled")) {
         element.style.color = "black";
         priorityLvlNode.removeAttribute("disabled");
-    }
-    else{
+    } else {
         element.style.color = "white";
         priorityLvlNode.disabled = true;
     }
@@ -69,8 +105,9 @@ function DeleteTask(element) {
     const userTaskNode = element.parentNode;
 
     userTaskParentNode.removeChild(userTaskNode);
-    
+
     localStorage.clear();
+
     localStorage.setItem("user_tasks", JSON.stringify(user_tasks.innerHTML));
 }
 
@@ -79,8 +116,9 @@ function DeleteSubTask(element) {
     const userSubTaskNode = element.parentNode;
 
     userSubTaskParentNode.removeChild(userSubTaskNode);
-        
+
     localStorage.clear();
+
     localStorage.setItem("user_tasks", JSON.stringify(user_tasks.innerHTML));
 }
 
@@ -90,7 +128,7 @@ function AddSubtask(element) {
         <!--
         An icon should be here.
         -->
-        <button aria-label="delete task" class="delete-task" onclick="DeleteSubTask(this)">Delete subtask</button>
+        <button aria-label="delete task" class="delete-subtask" onclick="DeleteSubTask(this)">Delete subtask</button>
 
         <input aria-label="Please enter your task" type = "text" class="user-todo" placeholder="Please input your task"/>
         
@@ -99,13 +137,13 @@ function AddSubtask(element) {
             <sunkanmii-progress-circle><span aria-label="progress">0</span>%</sunkanmii-progress-circle>
         </section>
 
-        <section>
+        <section class="stop-time">
             <label class="stop-time-label">Stop Time:</label>
             <input aria-labelledby="stop-time-label" class="custom-time" type="time">
             <time></time>
         </section>
             
-        <section>
+        <section class="save-subtask">
             <button class="done-button" onclick="SaveSubTask(this)">Done</button> 
         </section>
     </section>
@@ -126,6 +164,10 @@ function CompleteTask(element) {
 
     if (element.checked === true) {
         progressNum.textContent = 100;
+
+        //completeTasksDict[String()]
+
+        localStorage.setItem("tasks_checked");
         if (subtaskChildren.length != 2) {
 
             for (let i = 0; i < (subtaskChildrenLen - 2); i++) {
@@ -138,11 +180,13 @@ function CompleteTask(element) {
     } else {
         progressNum.textContent = 0;
 
-        if (subtaskChildren.length != 2) {
+        completeTasksDict[String(completeTasksCounter.increment)] = element.checked;
 
+        if (subtaskChildren.length != 2) {
             for (let i = 0; i < subtaskChildrenLen - 2; i++) {
                 for (let j = 0; j < 1; j++) {
                     subtaskChildren[i].children[2].checked = false;
+                    completeSubTasksDict[String(completeSubTasksCounter.increment)] = subtaskChildren[i].children[2].checked;
                     subtaskChildren[i].children[3].children[0].textContent = 0;
                 }
             }
@@ -152,28 +196,27 @@ function CompleteTask(element) {
     localStorage.setItem("user_tasks", JSON.stringify(user_tasks.innerHTML));
 }
 
-function CompleteSubtask(element){
+function CompleteSubtask(element) {
     const subtaskParentNode = element.parentNode;
     const subtaskAncestor = element.parentNode.parentNode.parentNode;
     const mainTaskCheckbox = subtaskAncestor.children[5];
     const allSubtasksLen = subtaskAncestor.children[8].children.length - 2;
     let subtaskCompletionQuota = 100 / allSubtasksLen;
-    
+
     const mainTaskCompletionPercentNode = subtaskAncestor.children[6].children[0];
     let mainTaskCompletionPercent = Number(mainTaskCompletionPercentNode.textContent);
-    
-    if(element.checked === true){
+
+    if (element.checked === true) {
         mainTaskCompletionPercent += subtaskCompletionQuota;
         subtaskParentNode.children[3].children[0].innerHTML = subtaskCompletionQuota.toPrecision(3);
         mainTaskCompletionPercentNode.innerHTML = mainTaskCompletionPercent.toPrecision(3);
-    }
-    else{
+    } else {
         mainTaskCompletionPercent -= subtaskCompletionQuota;
         subtaskParentNode.children[3].children[0].textContent = 0;
         mainTaskCompletionPercentNode.innerHTML = mainTaskCompletionPercent.toPrecision(3);
     }
 
-    if(Number(mainTaskCompletionPercentNode.innerHTML) === 100) {
+    if (Number(mainTaskCompletionPercentNode.innerHTML) === 100) {
         mainTaskCheckbox.checked = true;
     }
 
@@ -181,51 +224,65 @@ function CompleteSubtask(element){
 }
 
 function SaveTask(element) {
+    if(localStorage.getItem("user_tasks_values") !== null){
+        let dictInStor = JSON.parse(localStorage.getItem("user_tasks_values"));
+        myTasksDictFunc.setDict(dictInStor);
+        myTasksCounter.setValue(Object.keys(dictInStor).length);
+    }
+    
+    const myTasksDict = myTasksDictFunc.makeDict();
     const taskParentNode = element.parentNode.parentNode;
     const errorMessageNode = taskParentNode.children[1];
     const levelOfRelevanceNode = taskParentNode.children[0].children[1];
     const levelOfRelevanceValue = levelOfRelevanceNode.value;
+
+    myTasksCounter.increment();
+    myTasksDict[String(myTasksCounter.value())] = levelOfRelevanceValue;
     const userTaskNode = taskParentNode.children[3];
     const userTaskValue = userTaskNode.value;
+
+    myTasksCounter.increment();
+    myTasksDict[String(myTasksCounter.value())] = userTaskValue;
     const stopTimeNode = taskParentNode.children[5].children[1];
     const stopTimeValue = stopTimeNode.value;
+
+    myTasksCounter.increment();
+    myTasksDict[String(myTasksCounter.value())] = stopTimeValue;
     const completeTaskCheckBox = taskParentNode.children[4].children[0];
 
     try {
         if (userTaskValue === "" || stopTimeValue === "") {
             throw new InvalidArgumentException("Input cannot be empty.");
-        }
-        else if(element.textContent === "Edit"){
+        } else if (element.textContent === "Edit") {
             errorMessageNode.style.display = "none";
 
             levelOfRelevanceNode.removeAttribute("disabled");
             levelOfRelevanceNode.style.border = "2px solid rgb(238, 238, 238)";
-            
+
             userTaskNode.removeAttribute("disabled");
             userTaskNode.style.border = "2px solid rgb(238, 238, 238)";
-            
+
             stopTimeNode.removeAttribute("disabled");
             stopTimeNode.style.border = "2px solid rgb(238, 238, 238)";
-            
+
             completeTaskCheckBox.disabled = true;
-            
+
             element.textContent = "Done";
-        }
-        else{
+        } else {
             errorMessageNode.style.display = "none";
 
             levelOfRelevanceNode.disabled = true;
             levelOfRelevanceNode.value = levelOfRelevanceValue; //To save user's value in localStorage
             levelOfRelevanceNode.style.border = "none";
-            
+
             userTaskNode.disabled = true;
             userTaskNode.value = userTaskValue;
             userTaskNode.style.border = "none";
-            
+
             stopTimeNode.disabled = true;
             stopTimeNode.value = stopTimeNode.value;
             stopTimeNode.style.border = "none";
-            
+
             completeTaskCheckBox.removeAttribute("disabled");
 
             element.textContent = "Edit";
@@ -236,6 +293,7 @@ function SaveTask(element) {
     }
 
     localStorage.setItem("user_tasks", JSON.stringify(user_tasks.innerHTML));
+    localStorage.setItem("user_tasks_values", JSON.stringify(myTasksDict));
 }
 
 function SaveSubTask(element) {
@@ -243,34 +301,40 @@ function SaveSubTask(element) {
     const errorMessageNode = subTaskParentNode.children[2];
     const userSubTaskNode = subTaskParentNode.children[1];
     const userSubTaskValue = userSubTaskNode.value;
-    const stopTimeNode = subTaskParentNode.children[4].children[1];
+
+    mySubTasksCounter.increment();
+    mySubTasksDict[String(mySubTasksCounter.value())] = userSubTaskValue;
+
+    console.log(dictVals[1]);
+    const stopTimeNode = subTaskParentNode.children[3].children[1];
     const stopTimeValue = stopTimeNode.value;
+
+    mySubTasksCounter.increment();
+    mySubTasksDict[String(mySubTasksCounter.value())] = stopTimeValue;
     const completeSubTaskCheckBox = subTaskParentNode.children[2];
 
     try {
         if (userSubTaskValue === "" || stopTimeValue === "") {
             throw new Error("Parameter cannot be empty.");
-        }
-        else if(element.textContent === "Edit"){
+        } else if (element.textContent === "Edit") {
             userSubTaskNode.removeAttribute("disabled");
             userSubTaskNode.style.border = "2px solid rgb(238, 238, 238)";
-            
+
             stopTimeNode.removeAttribute("disabled");
             stopTimeNode.style.border = "2px solid rgb(238, 238, 238)";
-            
+
             completeSubTaskCheckBox.disabled = true;
 
             element.textContent = "Done";
-        }
-        else{
+        } else {
             userSubTaskNode.disabled = true;
             userSubTaskNode.style.border = "none";
-            
+
             stopTimeNode.disabled = true;
             stopTimeNode.style.border = "none";
-            
+
             completeSubTaskCheckBox.removeAttribute("disabled");
-            
+
             element.textContent = "Edit";
         }
     } catch (err) {
@@ -278,4 +342,31 @@ function SaveSubTask(element) {
     }
 
     localStorage.setItem("user_tasks", JSON.stringify(user_tasks.innerHTML));
+    localStorage.setItem("user_subtasks_values", JSON.stringify(mySubTasksDict));
+}
+
+
+function DisplayUserValues() {
+    if (localStorage.getItem("user_tasks_values") !== null && localStorage.getItem("user_subtasks_values") !== null) {
+        let dictVals = JSON.parse(localStorage.getItem("user_subtasks_values"));
+        let userSubTasksHTML = user_tasks.children;
+        userSubTasksHTML.children[1].value = dictVals[1];
+    } else if (localStorage.getItem("user_tasks_values") !== null) {
+        const dictVals = JSON.parse(localStorage.getItem("user_tasks_values"))
+        const userTasksLen = user_tasks.childElementCount / 2;
+
+        let j = 0;
+        for (let i = 0; i <= userTasksLen; i+=2) {
+            let cusItr = i + 1;
+            j++;
+            //Priority level node
+            user_tasks.children[cusItr].children[0].children[1].value = Number(dictVals[j]);
+            j++;
+            //User task node
+            user_tasks.children[cusItr].children[3].value = dictVals[j];
+            j++;
+            //Stop time node
+            user_tasks.children[cusItr].children[5].children[1].value = dictVals[j];
+        }
+    }
 }
